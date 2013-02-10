97f1dcb4c85fc68caa5ee36b67c2786adfb3d4c4
herze <herzed@gmail.com>
2013-02-10 20:16:20 +0100

    Base del Proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lostoys::Application.config.secret_token = '675891706bffd23efd2fe57bf84e5b67bc0309f69c09ba3e01316e0d0b29392ec036f39aa4f867bd63a2152dcec3fb9bdc37d8237002695854d761a5306e9414'
